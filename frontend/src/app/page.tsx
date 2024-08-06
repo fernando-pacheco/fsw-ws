0ec28f5b53b4f5523f961ba5216f567a4c455aa5
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-08-05 23:43:56 -0300

    [chore] initial commit - config

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+
+export default function Home() {
+  const [] = useState()
+  return (
+    <main>
+      <div className="flex justify-center items-center w-full h-screen m-5">
+        HelloWorld
+      </div>
+    </main>
+  );
+}
