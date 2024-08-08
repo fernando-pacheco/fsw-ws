dbde626510d44f27cf4f744b9170443e72d633f2
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-08-07 22:21:22 -0300

    [feat] signIn e signOut com o google

--- frontend/src/app/_components/sidebar-sheet.tsx
+++ frontend/src/app/_components/sidebar-sheet.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { House, CalendarDays, LogOut, LogIn } from "lucide-react";
 import { quickSearchOptions } from "../_constants/search";
 import { Button } from "./ui/button";
@@ -8,6 +10,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Separator } from "./ui/separator";
 import Link from "next/link";
 import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 interface SidebarSheetProps {
     children: ReactNode
@@ -16,6 +19,14 @@ interface SidebarSheetProps {
 export function SidebarSheet({
     children,
 }: SidebarSheetProps) {
+    const { data } = useSession()
+
+    async function handleLoginWithGoogle() {
+        await signIn("google")
+    }
+    async function handleLogoutWithGoogle() {
+        await signOut("google")
+    }
 
     return (
         <Sheet>
@@ -28,40 +39,46 @@ export function SidebarSheet({
                 </SheetHeader>
                 <div className="space-y-6">
                     {/* UserInfo */}
-                    <div className="flex items-center justify-between gap-4">
-                        <h2 className="font-bold">Olá, faça seu login!</h2>
-                        <Dialog>
-                            <DialogTrigger asChild>
-                                <Button size="icon">
-                                    <LogIn />
-                                </Button>
-                            </DialogTrigger>
-                            <DialogContent className="flex items-center justify-center flex-col w-[90%]">
-                                <DialogTitle>Faça login na plataforma</DialogTitle>
-                                <DialogDescription>Conecte-se usando sua conta Google.</DialogDescription>
-                                <Button
-                                    variant="outline"
-                                    className="w-full gap-2 border border-primary"
-                                >
-                                    <Image
-                                        alt="google"
-                                        src="/google.svg"
-                                        width={18}
-                                        height={18}
-                                    />
-                                    Google
-                                </Button>
-                            </DialogContent>
-                        </Dialog>
-                        {/* <Avatar className="border-2 border-primary size-14">
-                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
-                        <div>
-                            <p className="text-md">User Name</p>
-                            <p className="text-sm text-gray-400">name@email.com</p>
-                        </div> */}
-                    </div>
+                    {!data ? (
+                        <div className="flex items-center justify-between gap-4">
+                            <h2 className="font-bold">Olá, faça seu login!</h2>
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <Button size="icon">
+                                        <LogIn />
+                                    </Button>
+                                </DialogTrigger>
+                                <DialogContent className="flex items-center justify-center flex-col w-[90%]">
+                                    <DialogTitle>Faça login na plataforma</DialogTitle>
+                                    <DialogDescription>Conecte-se usando sua conta Google.</DialogDescription>
+                                    <Button
+                                        variant="outline"
+                                        className="w-full gap-2 border border-primary"
+                                        onClick={handleLoginWithGoogle}
+                                    >
+                                        <Image
+                                            alt="google"
+                                            src="/google.svg"
+                                            width={18}
+                                            height={18}
+                                        />
+                                        Google
+                                    </Button>
+                                </DialogContent>
+                            </Dialog>
+                        </div>
+                    ) : (
+                        <div className="flex items-center gap-4">
+                            <Avatar className="border-2 border-primary size-14">
+                                <AvatarImage src={data.user?.image ?? ''} alt="@shadcn" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <div>
+                                <p className="text-md">{data.user?.name}</p>
+                                <p className="text-sm text-gray-400">{data.user?.email}</p>
+                            </div>
+                        </div>
+                    )}
 
                     <Separator />
 
@@ -94,7 +111,11 @@ export function SidebarSheet({
                     <Separator />
 
                     {/* Logout */}
-                    <Button variant="ghost" className="w-full flex justify-start gap-3 hover:bg-primary">
+                    <Button
+                        variant="ghost"
+                        className="w-full flex justify-start gap-3 hover:bg-primary"
+                        onClick={handleLogoutWithGoogle}
+                    >
                         <LogOut size={18} />
                         Sair da conta
                     </Button>
